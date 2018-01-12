f92e25aa00f051974f0d537d4101c018d4031a28
Darian Nasrabadi <darian.nasrabadi@gmail.com>
2018-01-12 11:04:31 -0600

    finish 1-10

--- super_heroes.sql
+++ super_heroes.sql
@@ -60,29 +60,72 @@ SELECT * FROM heroes_powers;
 
 -- 1. How many powers are there?
 
+	SELECT count(powers.id) AS "Number of Unique Powers"
+	FROM powers;	
 
 -- 2. List all heroes and their powers. It's okay that heroes appear multiple times.
 
+	SELECT heroes.name, powers.name AS "super power"
+	FROM heroes
+	JOIN heroes_powers ON heroes.id = heroes_powers.hero_id
+	JOIN powers ON powers.id = heroes_powers.power_id
+	ORDER BY heroes.name;
 
 -- 3. Find all of Professor X's powers.
 
+	SELECT heroes.name, powers.name AS "super power"
+	FROM heroes
+	JOIN heroes_powers ON heroes.id = heroes_powers.hero_id
+	JOIN powers ON powers.id = heroes_powers.power_id
+	WHERE heroes.name = 'Professor X';
 
 -- 4. Add a new super power. 
 
+	INSERT INTO powers (name, description)
+	VALUES ('Rich', 'Has enough money to do/research/develop anything physically/theoretically possible');
 
 -- 5. Add this power to an existing hero.
 
+	INSERT INTO heroes_powers (hero_id, power_id, power_level)
+	VALUES (2, 9, 5);
 
 -- 6. Which heroes have laser vision? Include their name and alias.
 
-
+	SELECT heroes.name, heroes.alias, powers.name AS "super power"
+	FROM heroes
+	JOIN heroes_powers ON heroes.id = heroes_powers.hero_id
+	JOIN powers ON powers.id = heroes_powers.power_id
+	WHERE powers.name = 'Laser Vision';
+	
 -- 7. How many can fly? A twist on the above...
 
+	SELECT count(powers.name) AS "Heroes who can Fly"
+	FROM heroes
+	JOIN heroes_powers ON heroes.id = heroes_powers.hero_id
+	JOIN powers ON powers.id = heroes_powers.power_id
+	WHERE powers.name = 'Flight';
 
 -- 8. What is the average super strength power level?
 
+	SELECT avg(heroes_powers.power_level) AS "Average Super Strength Level"
+	FROM heroes
+	JOIN heroes_powers ON heroes.id = heroes_powers.hero_id
+	JOIN powers ON powers.id = heroes_powers.power_id
+	WHERE powers.name = 'Super Strength';
 
 -- 9. Show all the information for heroes with super strength power level over 2.
 
+	SELECT heroes.name, powers.name AS "super power", heroes_powers.power_level
+	FROM heroes
+	JOIN heroes_powers ON heroes.id = heroes_powers.hero_id
+	JOIN powers ON powers.id = heroes_powers.power_id
+	WHERE powers.name = 'Super Strength' AND heroes_powers.power_level > 2;
 
 -- 10. Find the average power level for all powers. Include the power name and descriptions.
+
+	
+	SELECT powers.name AS "super power", powers.description, avg(heroes_powers.power_level) AS "Average Super Strength Level"
+	FROM powers
+	JOIN heroes_powers ON powers.id = heroes_powers.power_id
+	GROUP BY powers.name, powers.description;
+
